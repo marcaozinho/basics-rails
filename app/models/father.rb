9fce93973b05fbec4b93a2083ff2d6a7cb977398
marcaozinho <aci.marcus@gmail.com>
2016-06-20 19:57:43 -0300

    Added high res paperclip

--- app/models/father.rb
+++ app/models/father.rb
@@ -5,7 +5,7 @@ class Father < ActiveRecord::Base
 	validates :name, presence: true
 	validates :email, presence: true, uniqueness:true
 
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", high: "1200x400>" }
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
 	def occupation_description
